ebbba1fc050a117856f6c82dbd245a0da2bd78d2
大树 <dashu@maihaoche.com>
2017-02-09 17:37:14 +0800

    Initial commit

---
+++ app/src/main/java/com/sz/bookkeeping/widget/CalendarView.java
@@ -0,0 +1,27 @@
+package com.sz.bookkeeping.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created with Android Studio.
+ * User: dashu
+ * Date: 2017/2/9
+ * Time: 下午5:29
+ * Desc:
+ */
+
+public class CalendarView extends FrameLayout {
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CalendarView(Context context) {
+        super(context);
+    }
+
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
